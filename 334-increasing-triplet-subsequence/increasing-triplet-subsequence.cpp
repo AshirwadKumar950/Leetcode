fd3573b7d798c61491ae4f8532d18a2f8f9645c4
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2025-12-22 23:48:18 +0530

    Time: 3 ms (41.31%) | Memory: 115.6 MB (66.83%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+         vector<int> tails;
+
+    for (int x : nums) {
+        auto it = lower_bound(tails.begin(), tails.end(), x);
+        if (it == tails.end())
+            tails.push_back(x);
+        else
+            *it = x;
+    }
+
+        return tails.size() >= 3;
+    }
+};
