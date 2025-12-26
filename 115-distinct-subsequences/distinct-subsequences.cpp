9907dcd46b150c7b6326eb43020b73e99e7a0ad6
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2025-12-26 22:09:53 +0530

    Time: 23 ms (74.22%) | Memory: 13 MB (77.05%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    string s,t;
-    int l1,l2;
-    int dp[1001][1001];
-    int rec(int i, int j){
-        if(j == l2) return 1;
-
-        if(i == l1){
-            return 0;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int ways = rec(i+1,j);
-        if(s[i] == t[j]){
-            ways += rec(i+1,j+1);
-        }
-        return dp[i][j] = ways;
-    }
+typedef long long ll;
     int numDistinct(string s, string t) {
-        this->s = s;
-        this->t = t;
-        this->l1 = s.length();
-        this->l2 = t.length();
-        memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+        int l1 = s.length();
+        int l2 = t.length();
+
+        int dp[1001][1001];
+        memset(dp,0,sizeof(dp));
+
+        for(int i = 0; i <= l1; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= l1; i++){
+            for(int j = 1; j <= l2; j++){
+                ll ways = dp[i-1][j];
+                if(s[i-1] == t[j-1]){
+                    ways += dp[i-1][j-1];
+                }
+                dp[i][j] = ways;
+            }
+        }
+        return dp[l1][l2];
     }
 };
