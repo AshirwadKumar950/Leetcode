fdc33a25f3c14950ebe6c0b1fbc8373da4c0c038
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2025-12-26 21:33:14 +0530

    Time: 4 ms (50.65%) | Memory: 9.1 MB (77.03%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     string s1,s2,s3;
     int l1,l2,l3;
-    int dp[101][101][202];
-    bool rec(int i, int j, int k){
-        if(i == l1 && j == l2 && k == l3) return true;
-        
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+    int dp[101][101];
+    bool rec(int i, int j){
+        if(i == l1 && j == l2) return true;
+        int k = i+j;
+        if(dp[i][j] != -1) return dp[i][j];
         //compare s3 with s1
         bool take = false;
         if(i < l1 && s3[k] == s1[i]){
-            take = take || rec(i+1,j,k+1);
+            take = take || rec(i+1,j);
         }
+        //compare s3 with s2
         if(j < l2 && s3[k] == s2[j]){
-            take = take || rec(i,j+1,k+1);
+            take = take || rec(i,j+1);
         }
-        return dp[i][j][k] = take;
+        return dp[i][j] = take;
     }
 
     bool isInterleave(string s1, string s2, string s3) {
@@ -27,6 +28,7 @@ public:
         this->l1 = s1.length();
         this->l2 = s2.length();
         this->l3 = s3.length();
-        return this->rec(0,0,0);
+        if(l1 + l2 != l3) return false; 
+        return this->rec(0,0);
     }
 };
