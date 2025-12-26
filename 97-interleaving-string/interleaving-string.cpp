2fd2fb72065528e069d9d45282ca12101c1e3e58
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2025-12-26 21:42:49 +0530

    Time: 4 ms (50.65%) | Memory: 9.5 MB (44.03%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    string s1,s2,s3;
-    int l1,l2,l3;
-    int dp[101][101];
-    bool rec(int i, int j){
-        if(i == l1 && j == l2) return true;
-        int k = i+j;
-        if(dp[i][j] != -1) return dp[i][j];
-        //compare s3 with s1
-        bool take = false;
-        if(i < l1 && s3[k] == s1[i]){
-            take = take || rec(i+1,j);
-        }
-        //compare s3 with s2
-        if(j < l2 && s3[k] == s2[j]){
-            take = take || rec(i,j+1);
-        }
-        return dp[i][j] = take;
-    }
-
     bool isInterleave(string s1, string s2, string s3) {
-        this->s1 = s1;
-        this->s2 = s2;
-        this->s3 = s3;  
-        memset(dp,-1,sizeof(dp));
-
-        this->l1 = s1.length();
-        this->l2 = s2.length();
-        this->l3 = s3.length();
+        int l1,l2,l3;
+        l1 = s1.length();
+        l2 = s2.length();
+        l3 = s3.length();
         if(l1 + l2 != l3) return false; 
-        return this->rec(0,0);
+
+        bool dp[101][101];
+        memset(dp,false,sizeof(dp));
+
+        dp[0][0]= true;
+
+        for(int i = 1; i <= l1; i++){
+            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        }
+
+        for(int j = 1; j <= l2; j++){
+            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
+        }
+
+        for(int i = 1; i <= l1; i++){
+            for(int j = 1; j <= l2; j++){
+                int k = i+j-1;
+                bool take = false;
+                if(s3[k] == s1[i-1]){
+                    take = take || dp[i-1][j];
+                }
+                if(s3[k] == s2[j-1]){
+                    take = take || dp[i][j-1];
+                }
+                dp[i][j] = take;
+            }
+        }
+
+        return dp[l1][l2];
     }
 };
