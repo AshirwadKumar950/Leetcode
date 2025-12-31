7c1958069d6523d7c466a9721375597cb45d26b8
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2025-12-31 13:02:41 +0530

    Time: 13 ms (40.48%) | Memory: 34 MB (34.41%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
+    vector<int>nums;
+    int n;
+    int dp[100001][4];
+    int rec(int i, int mod){
+        if(i == n){
+            if(mod == 0) return 0;
+            return INT_MIN;
+        }
+        if(dp[i][mod] != -1) return dp[i][mod];
+        int notcon = rec(i+1,mod);
+        int cons = nums[i] + rec(i+1,(mod+nums[i])%3);
+        return dp[i][mod] = max(notcon,cons);
+    }
     int maxSumDivThree(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        if(sum % 3 == 0) return sum;
-        int n = nums.size();
-        vector<int>v1,v2;
-        for(auto &v : nums){
-            if(v%3==1) v1.push_back(v);
-            else if(v%3==2) v2.push_back(v);
-        }
-
-        
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(), v2.end());
-        int mod = sum%3;
-        int ans = 0;
-        if(mod == 1){
-            if(v1.size() >= 1) ans = max(ans,sum-v1[0]);
-            if(v2.size() > 1)  ans = max(ans,sum-v2[0]-v2[1]);
-        }else if(mod == 2){
-            if(v2.size() >= 1) ans = max(ans,sum-v2[0]);
-            if(v1.size() > 1) ans = max(ans,sum-v1[0]-v1[1]);
-        }
-        return ans;
+        this->nums = nums;
+        this->n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0);
     }
 };
