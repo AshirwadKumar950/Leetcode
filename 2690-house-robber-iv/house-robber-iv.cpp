9a3dacfe165d4dd5289fe64c5b0354f5e655e84c
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-10 02:59:12 +0530

    Time: 13 ms (96.64%) | Memory: 60.9 MB (63.24%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    //the robber needs to steel from atleast k houses
+    bool check(vector<int>&nums, int k, int mid){
+        //now i need to check if i can steel 
+        //at least k non-consecutive houses 
+        //with atleast mid value
+        int n = nums.size();
+        int i = 0;
+        while(i < n){
+            if(nums[i] <= mid){
+                k--;
+                i += 2;
+            }else{
+                i++;
+            }
+        }
+        return k <= 0;
+    }
+    int minCapability(vector<int>& nums, int k) {
+        int lb = *min_element(nums.begin(),nums.end()), ub = *max_element(nums.begin(),nums.end());
+        int ans = 0;
+        while(lb <= ub){
+            int mid = lb + (ub-lb)/2;
+            if(check(nums,k,mid)){
+                ans = mid;
+                ub = mid - 1;
+            }else{
+                lb = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
