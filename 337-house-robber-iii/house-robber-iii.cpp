4e80588c0e7b5bad0e500971d37ff74d20332d85
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-10 02:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (53.20%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -11,25 +11,18 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*,int>mp;
-    int rec(TreeNode* root){
-        if(!root) return 0;
+    pair<int,int>rec(TreeNode* root){
+        if(!root) return {0,0};
 
-        if(mp.count(root)) return mp[root];
-        int nottake = rec(root->left) + rec(root->right);
+        auto left = rec(root->left);
+        auto right = rec(root->right);
 
-        int take = root->val;
-        if(root->left){
-            take += rec(root->left->left);
-            take += rec(root->left->right);
-        }
-        if(root->right){
-            take += rec(root->right->left);
-            take += rec(root->right->right);
-        }
-        return mp[root] = max(take,nottake);
+        int rob =  root->val + left.second + right.second;
+        int dontrob = max(left.first,left.second) + max(right.first,right.second);
+        return {rob,dontrob};
     }
     int rob(TreeNode* root) {
-        return rec(root);
+        auto v =  rec(root);
+        return max(v.first,v.second);
     }
 };
