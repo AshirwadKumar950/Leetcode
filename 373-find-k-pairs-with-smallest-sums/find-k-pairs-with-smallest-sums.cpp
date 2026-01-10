7d2207a4f2a4e05b49504b52d16d1d691e32717d
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-11 00:46:51 +0530

    Time: 32 ms (94.51%) | Memory: 134.2 MB (90.23%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -5,16 +5,19 @@ public:
         priority_queue<piii,vector<piii>,greater<piii>>pq;
         int n1 = nums1.size();
         int n2 = nums2.size();
-        for(int i = 0; i < min(1000,n1); i++){
-            for(int j = 0; j < min(1000,n2); j++){
-                pq.push({nums1[i]+nums2[j],{i,j}});
-            }
+        if(n1 == 0 || n2 == 0) return {};
+        for(int i = 0; i < min(k,n1); i++){
+            pq.push({nums1[i]+nums2[0],{i,0}});
         } 
         vector<vector<int>>ans;
         while(k--){
             piii tp = pq.top();
             pq.pop();
-            ans.push_back({nums1[tp.second.first],nums2[tp.second.second]});
+            int i = tp.second.first, j = tp.second.second;
+            ans.push_back({nums1[i],nums2[j]});
+            if(j+1 < n2){
+                pq.push({nums1[i]+nums2[j+1],{i,j+1}});
+            }
         }
         return ans;
     }
