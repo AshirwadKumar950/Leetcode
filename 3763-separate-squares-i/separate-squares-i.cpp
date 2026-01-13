3f383ca628d74fed1c32f34887a0b9f091c2106b
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-13 09:21:32 +0530

    Time: 144 ms (29.48%) | Memory: 198.7 MB (73.99%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    pair<double,double>get(vector<vector<int>>&squares, double mid){
+        double downArea = 0, upArea = 0;
+        for(auto &square : squares){
+            double x = square[0], y = square[1], l = square[2];
+
+            if(y + l <= mid){
+                downArea += l*l;
+            }else if(y >= mid){
+                upArea += l*l;
+            }else{
+                double downLength = mid - y;
+                double upLength = (y+l)-mid;
+                downArea += downLength*l;
+                upArea += upLength*l;
+            }
+        }
+        return {upArea,downArea};
+    }
+    double separateSquares(vector<vector<int>>& squares) {
+        double totalArea = 0;
+        double lb = 0, ub = 2e18+1;
+        while(ub - lb > 1e-5){
+            //mid is the mid line where i will check
+            //area at top of this and area at bottom of this
+            double mid = lb + (ub-lb)/2;
+            auto [up,down] = get(squares,mid);
+            if(up > down){
+                lb = mid;
+            }else{
+                ub = mid;
+            }
+        }
+        return lb;
+    }
+};
