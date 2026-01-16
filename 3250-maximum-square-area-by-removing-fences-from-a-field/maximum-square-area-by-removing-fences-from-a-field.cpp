4de9ed5ef09d10c8f747b8a8b2cefabdea915c9b
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-16 06:26:29 +0530

    Time: 1021 ms (66.99%) | Memory: 267.1 MB (55.34%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -4,12 +4,13 @@ public:
     int maximizeSquareArea(int m, int n, vector<int>& h, vector<int>& v) {
         //the values which are not present in both is already common for both
         //we need to find max common i can get from both
-        unordered_set<int>v1,v2;
+        unordered_set<int>v1;
         h.push_back(1);
         h.push_back(m);
         v.push_back(1);
         v.push_back(n);
         int s1 = h.size(), s2 = v.size();
+        int ans = INT_MIN;
         for(int i = 0; i < s1; i++){
             for(int j = i+1; j < s1; j++){
                 v1.insert(abs(h[i]-h[j]));
@@ -17,13 +18,8 @@ public:
         }
         for(int i = 0; i < s2; i++){
             for(int j = i+1; j < s2; j++){
-                v2.insert(abs(v[i]-v[j]));
-            }
-        }
-        int ans = INT_MIN;
-        for(auto &val : v1){
-            if(v2.find(val) != v2.end()){
-                ans = max(ans,val);
+                int val = abs(v[i]-v[j]);
+                if(v1.find(val) != v1.end()) ans = max(ans,val);
             }
         }
         return ans == INT_MIN ? -1 : 1ll*ans*ans%MOD;
