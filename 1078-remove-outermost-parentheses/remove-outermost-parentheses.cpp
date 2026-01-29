b9a315ee12b13767b6c809f6c4b64b0e05562348
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-01-29 13:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (53.78%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         int l = s.length();
-        int c1 = 0, c2 = 0;
-        int k = 0;
-        unordered_set<int>st;
-        for(int i = 0; i < l; i++){
-            if(s[i] == '(') c1++;
-            if(s[i] == ')') c2++;
-            if(c1 != 0 && c1 == c2){
-                st.insert(k);
-                st.insert(i);
-                if(i+1 < l) k = i+1;
-                c1 = 0;c2 = 0;
-            }
-        }
+        int c1 = 0;
         string ans ="";
         for(int i = 0; i < l; i++){
-            if(st.find(i) == st.end()) ans += s[i];
+            if(s[i] == '('){
+                if(c1 > 0) ans += s[i];
+                c1++;
+            }else{
+                c1--;
+                if(c1 > 0) ans += s[i];
+            }
         }
         return ans;
     }
