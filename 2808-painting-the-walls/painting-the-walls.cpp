e3d534c5333ffc9dbb77384b1e8f76015c176025
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-03-06 23:10:32 +0530

    Time: 39 ms (79.47%) | Memory: 121.1 MB (58.60%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -3,19 +3,21 @@ public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
         const int INF = 1e9;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,INF));
+        vector<int>dp(n+1,INF);
         for(int i = 0; i <= n; i++){
-            dp[i][n] = 0;
+            dp[n] = 0;
         }
         for(int i = n-1; i >= 0; i--){
+            vector<int>curr(n+1,INF);
             for(int used = 0; used <= n; used++){
-                int dont = dp[i+1][used];
+                int dont = dp[used];
                 int next = min(n,used+1+time[i]);
-                int paint = cost[i] + dp[i+1][next];
+                int paint = cost[i] + dp[next];
 
-                dp[i][used] = min(dont, paint);
+                curr[used] = min(dont, paint);
             }
+            dp = curr;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
