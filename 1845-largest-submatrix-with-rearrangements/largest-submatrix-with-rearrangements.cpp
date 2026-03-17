d7b8f8316f006c3091a4240c49656721f3117474
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-03-17 08:43:10 +0530

    Time: 29 ms (72.87%) | Memory: 79.7 MB (96.28%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -5,20 +5,19 @@ public:
         int n = matrix[0].size();
         // number of cons 1's ending at each position
 
-        vector<vector<int>>pre(m,vector<int>(n));
         for(int i = 0; i < n; i++){
             int cnt = 0;
             for(int j = m-1; j >= 0; j--){
                 if(matrix[j][i] == 1) cnt++;
                 else cnt = 0;
-                pre[j][i] = cnt;
+                matrix[j][i] = cnt;
             }
         }
         int ans = 0;
         for(int i = 0; i < m; i++){
-            sort(pre[i].begin(),pre[i].end());
+            sort(matrix[i].begin(),matrix[i].end());
             for(int j = 0; j < n; j++){
-                ans = max(ans,pre[i][j]*(n - j));
+                ans = max(ans,matrix[i][j]*(n - j));
             }
         }
         return ans;
