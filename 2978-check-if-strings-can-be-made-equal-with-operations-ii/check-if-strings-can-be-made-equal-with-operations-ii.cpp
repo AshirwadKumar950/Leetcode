0307b09e176a20fdf4998551a1dfe0526540d8fa
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-03-29 17:00:54 +0530

    Time: 49 ms (32.00%) | Memory: 23.5 MB (28.67%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        unordered_map<char,int>mp1,mp2;
+        int l = s1.length();
+        for(int i = 0; i < l; i++){
+            if(i%2 == 0) mp1[s1[i]]++;
+            else mp2[s1[i]]++;
+        }
+        for(int i = 0; i < l; i++){
+            if(i%2==0) mp1[s2[i]]--;
+            else mp2[s2[i]]--;
+
+            if(mp1[s2[i]]==0) mp1.erase(s2[i]);
+            if(mp2[s2[i]]==0) mp2.erase(s2[i]);
+        }
+        return mp1.empty() && mp2.empty();
+    }
+};
