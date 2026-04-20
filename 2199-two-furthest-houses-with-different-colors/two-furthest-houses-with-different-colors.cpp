aef2c3de2b9f297ea9e0ff571d8ba4272905d4fd
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-04-20 07:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (72.82%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -3,14 +3,26 @@ public:
     int maxDistance(vector<int>& colors) {
         int n = colors.size();
         int ans = INT_MIN;
-        unordered_map<int,int>mp;
-        for(int i = 0; i < n; i++){
-            int color = colors[i];
-            for(auto &[c,idx] : mp){
-                if(c != color) ans = max(ans,(i-idx));
+        // assume first index is first element an then last
+        // index is first element
+        int ridx = INT_MIN;
+        for(int i = n-1; i > 0; i--){
+            if(colors[i] != colors[0]){
+                ridx = i;
+                break;
             }
-            if(mp.find(color) == mp.end()) mp[color] = i;
         }
+        if(ridx == INT_MIN) return -1;
+        ans = max(ans,(ridx));
+        int lidx = INT_MIN;
+        for(int i = 0; i < n-1; i++){
+            if(colors[i] != colors[n-1]){
+                lidx = i;
+                break;
+            }
+        }
+        if(lidx == INT_MIN) return -1;
+        ans = max(ans,(n-1-lidx));
         return ans;
     }
 };
