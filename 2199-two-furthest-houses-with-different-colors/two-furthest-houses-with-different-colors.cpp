3421220e5f55d538855149db21cf629f0a81f849
Ashirwad_Kumar <ashirwadsingh950@gmail.com>
2026-04-20 07:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (6.97%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -5,13 +5,11 @@ public:
         int ans = INT_MIN;
         unordered_map<int,int>mp;
         for(int i = 0; i < n; i++){
-            int firstColor = colors[i];
-            for(int j = i+1; j < n; j++){
-                int secondColor = colors[j];
-                if(firstColor != secondColor){
-                    ans = max(ans,j-i);
-                }
+            int color = colors[i];
+            for(auto &[c,idx] : mp){
+                if(c != color) ans = max(ans,(i-idx));
             }
+            if(mp.find(color) == mp.end()) mp[color] = i;
         }
         return ans;
     }
